46796a215ca1a5f530b260163252dd681997b1f6
MacYanis <yanisjdz@outlook.fr>
2021-10-13 16:27:08 +0200

    Single Article View

---
+++ src/views/Article.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="article">
+        <ArticleComponent/>
+    </div>
+  
+</template>
+
+<script>
+import ArticleComponent from '../components/ArticleComponent.vue'
+
+export default {
+    name:'Article',
+    components:{
+        ArticleComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
