783308d8652ceb98f3939ba819487b83a8ec9b2c
robLittiere <robinl.95@orange.fr>
2021-10-14 10:34:28 +0200

    Basic app changes with router + Header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>   
-    </div>
+    <HeaderPreview />
+    
     <router-view/>
   </div>
 </template>
+<script>
+import HeaderPreview from '@/components/HeaderPreview.vue'
 
+export default {
+  components: {
+    HeaderPreview,
+  }  
+}
+</script>
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lobster&family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300;1,400;1,500&family=Secular+One&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
 }
 
 #nav {
