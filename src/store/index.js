c931cb163f2d1298f4856bfc615fdd676251efa1
MacYanis <yanisjdz@outlook.fr>
2021-10-14 10:27:45 +0200

    Axios get example

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+const axios = require('axios');
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    articles : []
   },
   mutations: {
+    getApi(state){
+      axios.get('http://api.fakecompany.life:30002/news')
+              .then(result => {
+                result.data.forEach(article => {
+                  state.articles.push(article)
+                });
+                console.log(state.articles);
+              })
+    }
   },
   actions: {
   },
