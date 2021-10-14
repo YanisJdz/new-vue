552572d924c9a2031b555319776e41c32511d985
robLittiere <robinl.95@orange.fr>
2021-10-14 15:39:57 +0200

    Deleted Article Preview to create a ArticleList and Aritcle Item components

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="row">
-        <div v-for="(article, index) in getArticles" :key="index" class="blog-article d-flex justify-content-center flex-column align-items-center mb-3 col-md-6">
+        <div class="blog-article d-flex justify-content-center flex-column align-items-center mb-3 col-md-6">
             <div class="article-container d-flex justify-content-center flex-column">
                 <div class="image-container mb-2">
                     <img v-if="article.image_url" :src='article.image_url' :alt="article.title">  
-                    <img v-else src="https://image.freepik.com/vecteurs-libre/collection-joueurs-football-dessin-anime_23-2149022199.jpg" :alt="article.title">
+                    <img v-else :src='article.urlToImage' :alt="article.title">
                 </div>
                 <h2 class="article-title">{{ article.title }} ?</h2>
                 <p class="article-date">{{ new Date(article.pubDate).toLocaleString("fr-FR", {
@@ -13,6 +13,7 @@
                     year: 'numeric'
                 }) }} - 4 min read</p>
                 <p class="article-description"> {{ article.description }} </p>
+                
             </div>
         </div>
     </div>
@@ -20,15 +21,13 @@
 
 <script>
 export default {
-    name: "ArticlePreview",
+    name: "ArticleItem",
+    props: {
+        article: Object
+    },
     data() {
         return {
             
-        }
-    },
-    computed: {
-        getArticles(){
-            return this.$store.state.articles
         }
     },
     methods: {
