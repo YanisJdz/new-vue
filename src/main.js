930ba3c57d0835d4c57565636b28b463be92d4ed
MacYanis <yanisjdz@outlook.fr>
2021-10-14 14:36:32 +0200

    add FA icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import "bootstrap/dist/css/bootstrap.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFeatherAlt,faUserSecret,faAt,faHistory,faKey} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFeatherAlt,faUserSecret,faAt,faHistory,faKey)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
