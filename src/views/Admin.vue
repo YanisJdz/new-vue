fbc517ac0cc292dd4005574da36a0c9c5a120920
Astronote <mathisouchikh@gmail.com>
2021-10-15 09:20:44 +0200

    Update style on the admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,15 +2,17 @@
     <div class='container d-flex flex-column'>
         <div class="admin-panel d-flex flex-column">
             <h1 class="mb-3 mt-3">Vue Admin</h1>
-            <p class = "subtitle">Author</p>
-            <input class="input-area" type="text" v-model="author" placeholder="Author">
-            <p class = "subtitle">Title</p>
-            <input class="input-area" type="text" v-model="title" placeholder="Title">
-            <p class = "subtitle">Description</p>
-            <textarea class="input-area" v-model="description" name="description" id="1" cols="1" rows="2" placeholder="Description of the article"></textarea>
-            <p class = "subtitle">Content</p>
-            <textarea  class="input-area" v-model="content" name="content" id="" cols="30" rows="10" placeholder="Content of the article goes right here"></textarea>
-            <button @click="addArticle(createArticle())" v-bind:disabled="author.length === 0" type="submit">Add article</button>
+            <div>
+            <p class = "subtitle">Author :</p>
+            <input class="input-area form-control" type="text" v-model="author" placeholder="Author">
+            </div>
+            <p class = "subtitle">Title :</p>
+            <input class="input-area form-control" type="text" v-model="title" placeholder="Title">
+            <p class = "subtitle">Description :</p>
+            <textarea class="input-area form-control" v-model="description" name="description" id="1" cols="1" rows="2" placeholder="Description of the article"></textarea>
+            <p class = "subtitle">Content :</p>
+            <textarea  class="input-area form-control" v-model="content" name="content" id="" cols="30" rows="10" placeholder="Content of the article goes right here"></textarea>
+            <button class="btn btn-primary" @click="addArticle(createArticle())" v-bind:disabled="author.length === 0" type="submit">Add article</button>
         </div>
         <div class="preview-article">
             <h2 class="mt-3 mb-3">Article Preview</h2>
@@ -54,17 +56,18 @@ export default {
 
 <style>
 h1{
-    text-decoration: underline;
+    text-decoration: underline #001252;
 }
 
 button {
     margin-top: 10px;
-    border: black;
-    
 }
 .subtitle{
+    font-size: clamp(12px, 1.5vw, 18px);
     display: flex;
-    margin-top: 2px;
+    margin-top: 10px;
+    margin-bottom: 0;
+    text-decoration: underline #001252;
 }
 
 </style>
