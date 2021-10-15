8996e5f7f458a72b0e25858495facc9b109fb641
Astronote <mathisouchikh@gmail.com>
2021-10-15 10:48:05 +0200

    Filling up blank pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About us</h1>
+    <h4>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et viverra lectus. In eu mattis sem. Mauris lacinia nisi quis enim ultricies sollicitudin. Praesent mattis sagittis arcu, id interdum arcu finibus in. Curabitur quis faucibus purus. Aliquam porttitor fringilla turpis, et posuere ipsum molestie non. In vestibulum mi non felis placerat tincidunt ut pretium purus. In tincidunt gravida risus, ut consectetur ligula pellentesque ut. 
+    </h4>
   </div>
 </template>
+
+<style>
+h1{
+  
+  margin-top: 2%;
+}
+h4{
+  text-align: start;
+  margin-top: 5%;
+  margin-left: 2%;
+  margin-right: 2%;
+}
+</style>
