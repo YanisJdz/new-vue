b55992bbb0ca4e8d9500f71a8b5b5dd13cb44746
MacYanis <yanisjdz@outlook.fr>
2021-10-15 12:04:00 +0200

    Refactoring admin vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,61 +1,50 @@
 <template>
-    <div class='container d-flex flex-column'>
-        <div class="admin-panel d-flex flex-column">
-            <h1 class="mb-3 mt-3">Vue Admin</h1>
-            <p class = "subtitle">Author</p>
-            <input class="input-area" type="text" v-model="author" placeholder="Author">
-            <p class = "subtitle">Title</p>
-            <input class="input-area" type="text" v-model="title" placeholder="Title">
-            <p class = "subtitle">Description</p>
-            <textarea class="input-area" v-model="description" name="description" id="1" cols="1" rows="2" placeholder="Description of the article"></textarea>
-            <p class = "subtitle">Content</p>
-            <textarea  class="input-area" v-model="content" name="content" id="" cols="30" rows="10" placeholder="Content of the article goes right here"></textarea>
-            <button @click="addArticle(createArticle())" v-bind:disabled="author.length === 0" type="submit">Add article</button>
-        </div>
-        <div class="preview-article">
-            <h2 class="mt-3 mb-3">Article Preview</h2>
-            <h2>{{ author }}</h2>
-            <h3>{{ title }}</h3>
-            <p class='articleDescription'>{{ description}}</p>
-            <p class='articleContent'>{{ content}}</p>
-            
-        </div>
-    
+<div class="admin">
+    <div>
+        <button v-for="(page,index) in pages" :key="index" 
+        @click="currentPage = index">
+        {{page.buttonName}}
+        </button>
     </div>
+    <div>
+        <component :is="computedPage"/>
+    </div>
+</div>
 </template>
 
 <script>
+
+import AdminAddComponent from '../components/AdminAddComponent.vue'
+import AdminModifyComponent from '../components/AdminModifyList.vue'
 export default {
+
     name: 'Admin',
-    data() {
-        return {
-            title: '',
-            author: '',
-            description: '',
-            content: '',
-        }
-    },
-    methods: {
-        createArticle(){
-            let article = {
-                title: this.title,
-                author: this.author,
-                description: this.description,
-                content: this.content
-            }
-            return article
-        },
-        addArticle(article){
-            this.$store.commit('addArticle', article)
-        }
-    }
+    data(){
+       return{
+           currentPage: 0,
+           pages:[
+               {
+                    name:'AdminAddComponent',
+                    compo: AdminAddComponent,
+                    buttonName:'Ajouter un article'
+               },
+               {
+                    name:'AdminModifyComponent',
+                    compo: AdminModifyComponent,
+                    buttonName:'Modifier ou supprimer un article'
+               }
+           ]
+       }
+   },
+   computed:{
+       computedPage(){
+           return this.pages[this.currentPage].compo
+       }
+   }
 }
 </script>
 
 <style>
-h1{
-    text-decoration: underline;
-}
 
 button {
     margin-top: 10px;
@@ -64,7 +53,8 @@ button {
 }
 .subtitle{
     display: flex;
-    margin-top: 2px;
+    margin-top: 10px;
+    margin-bottom: 0px;
 }
 
 </style>
